cc0ab72d995db4baa848483ab6807aa78047920c
David <david@david-Lenovo-U310>
2016-09-05 14:48:25 -0500

    base de datos completa, canciones con salto de linea y revision de ortografia

--- integrantes/models.py
+++ integrantes/models.py
@@ -4,3 +4,9 @@ class Integrante(models.Model):
 	nombre = models.CharField(max_length=10)
 	rol = models.CharField(max_length=20)
 	foto = models.FileField(upload_to='integrante/%Y/%m/%d/', null=True, blank=True)
+
+	def __str__(self):
+		return self.nombre
+
+	class Meta:
+		ordering = ['id']
