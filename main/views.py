21ae30014e02fab9c3f599673a52592e60b078db
David <david@david-Lenovo-U310>
2016-12-16 11:52:51 -0600

    biografia 4

--- main/views.py
+++ main/views.py
@@ -9,8 +9,8 @@ class Home(View):
 class Bio(View):
 	def get(self, request):
 		template_name = 'main/bio.html'
-		banda = Banda.objects.get(pk=1 or None)
+		bandas = Banda.objects.all()
 		context = {
-			'banda': banda
+			'bandas': bandas
 		}
 		return render(request, template_name, context)
